480adb96d5721fc3f67b86b0ff262f5efb332b04
HoangNgoVan <ngovanhoang.2110@gmail.com>
2024-07-11 11:37:17 +0700

    Add Sort Details Search For StoreController

--- BookStoreMVC/Controllers/StoreController.cs
+++ BookStoreMVC/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using BookStoreMVC.Models;
 using BookStoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreMVC.Controllers
@@ -14,14 +15,51 @@ namespace BookStoreMVC.Controllers
         {
             this.context = context;
         }
-        public async Task<IActionResult> Index(int pageIndex)
+        public async Task<IActionResult> Index(int pageIndex, string? search, string? brand, string? category, string? sort)
         {
-            IQueryable<Product> query = context.Products;
+            //select join
+            var query = from p in context.Products
+                        join c in context.Categorys on p.CategoryId equals c.CategoryId
+                        join b in context.Brands on p.BrandId equals b.BrandId into pcb
+                        from b in pcb.DefaultIfEmpty()
+                        select new { p, c, b };
 
-            query = query.OrderByDescending(p => p.Id);
+
+            // search functionality
+            if (search != null && search.Length > 0) 
+            {
+                query = query.Where(p => p.p.BookName.Contains(search));
+            }
+
+            // filter functionality
+            if (brand != null && brand.Length > 0) 
+            {
+                query = query.Where(p => p.b.BrandName.Contains(brand));
+            }
+
+            if (category != null && category.Length > 0) 
+            {
+                query = query.Where(p => p.c.CategoryName.Contains(category));
+            }
+
+            // sort functionality
+            if(sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.p.Price);
+            }
+            else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.p.Price);
+            }
+            else
+            {
+                // newest products first
+                query = query.OrderByDescending(p => p.p.Id);
+            }
+
 
             //pagination functionality
-            if(pageIndex <1)
+            if (pageIndex < 1)
             {
                 pageIndex = 1;
             }
@@ -30,13 +68,83 @@ namespace BookStoreMVC.Controllers
             int totalPages = (int)Math.Ceiling(count / pageSize);
             query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
 
-            var products = await query.ToListAsync();
+            //Create Product View
+            var products = await query.Select(x => new ProductsVm()
+            {
+                Id = x.p.Id,
+                BookName = x.p.BookName,
+                BrandName = x.b.BrandName,
+                Author = x.p.Author,
+                CategoryName = x.c.CategoryName,
+                Price = x.p.Price,
+                Description = x.p.Description,
+                ImageFileName = x.p.ImageFileName,
+                CreatedAt = x.p.CreatedAt,
+                Stock = x.p.Stock,
+                IsStock = x.p.IsStock,
+                NumberOfPage = x.p.NumberOfPage,
+                PublishYear = x.p.PublishYear,
+            }).ToListAsync();
 
             ViewBag.Products = products;
-            ViewBag.TotalPages = totalPages;   
+
+            ViewBag.TotalPages = totalPages;
             ViewBag.PageIndex = pageIndex;
 
-            return View();
+            IEnumerable<SelectListItem> CategoryList = context.Categorys.
+                Select(c => new SelectListItem
+                {
+                    Text = c.CategoryName,
+                    Value = c.CategoryName
+                });
+            ViewBag.CategoryList = CategoryList;
+
+            IEnumerable<SelectListItem> BrandList = context.Brands
+             .Select(b => new SelectListItem
+             {
+                 Text = b.BrandName,
+                 Value = b.BrandName
+             });
+            ViewBag.BrandList = BrandList;
+
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Brand = brand,
+                Category = category,
+                Sort = sort
+            };
+
+
+            return View(storeSearchModel);
+        }
+
+        public  async Task<IActionResult> Details(int id)
+        {
+            //select join
+            var productById = await (from p in context.Products
+                        join c in context.Categorys on p.CategoryId equals c.CategoryId
+                        join b in context.Brands on p.BrandId equals b.BrandId into pcb
+                        from b in pcb.DefaultIfEmpty()
+                        where p.Id == id
+                        select new ProductsVm
+                        {
+                            Id = p.Id,
+                            BookName = p.BookName,
+                            BrandName = b.BrandName,
+                            Author = p.Author,
+                            CategoryName = c.CategoryName,
+                            Price = p.Price,
+                            Description = p.Description,
+                            ImageFileName = p.ImageFileName,
+                            CreatedAt = p.CreatedAt,
+                            Stock = p.Stock,
+                            IsStock = p.IsStock,
+                            NumberOfPage = p.NumberOfPage,
+                            PublishYear = p.PublishYear,
+                        }).FirstOrDefaultAsync();
+
+            return View(productById);
         }
     }
 }
